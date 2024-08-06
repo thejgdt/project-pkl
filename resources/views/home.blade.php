b35dda351062fd5fb1393fc2d6f7d61cf3845962
thejgdt <theaberchio@gmail.com>
2024-08-06 21:19:08 +0700

    component: initialize `section` - make <section /> into component 	- this will shorten the code writing by only having to call the section component 'x-section' without having to write it manually repeatedly

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,35 +1,33 @@
 <x-app-layout>
     <div class="max-w-7xl mx-auto px-6 sm:px-8">
         <!-- Hero -->
-        <section id="hero" class="h-screen flex items-center justify-center">
-            <div class="mx-auto my-auto max-w-3xl pt-16 sm:pt-24 lg:pt-32 py-8 sm:py-16 lg:py-24">
-                <div class="hidden sm:mb-8 sm:flex sm:justify-center">
-                    <div
-                        class="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 dark:ring-gray-500 dark:text-gray-400 hover:ring-gray-900/20 dark:hover:ring-gray-200/20">
-                        Latest from the blog. <a href="#"
-                            class="font-semibold text-indigo-500 dark:hover:text-indigo-400 hover:underline"><span
-                                class="absolute inset-0" aria-hidden="true"></span>Read more <span
-                                aria-hidden="true">&rarr;</span></a>
-                    </div>
-                </div>
-                <div class="text-center">
-                    <h1 class="text-4xl font-bold tracking-tight text-gray-900 dark:text-gray-200 sm:text-6xl">Data to
-                        enrich your online business</h1>
-                    <p class="mt-6 text-lg leading-8 text-gray-600 dark:text-gray-400">Lorem ipsum dolor sit amet
-                        consectetur adipisicing elit. Iure debitis odio libero nihil! Aspernatur delectus natus aperiam,
-                        consectetur repudiandae consequuntur quidem omnis cupiditate minima repellat dolorum neque
-                        doloremque odio tempore.</p>
-                    <div class="mt-10 flex items-center justify-center gap-x-6">
-                        <button
-                            class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                            Get started
-                        </button>
-                        <a href="#"
-                            class="text-sm font-semibold leading-6 text-gray-900 dark:text-gray-400 hover:text-indigo-600 dark:hover:text-indigo-400 hover:underline">Learn
-                            more <span aria-hidden="true">&rarr;</span></a>
-                    </div>
+        <x-section>
+            <div class="hidden sm:mb-8 sm:flex sm:justify-center">
+                <div
+                    class="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 dark:ring-gray-500 dark:text-gray-400 hover:ring-gray-900/20 dark:hover:ring-gray-200/20">
+                    Latest from the blog. <a href="#"
+                        class="font-semibold text-indigo-500 dark:hover:text-indigo-400 hover:underline"><span
+                            class="absolute inset-0" aria-hidden="true"></span>Read more <span
+                            aria-hidden="true">&rarr;</span></a>
                 </div>
             </div>
-        </section>
+            <div class="text-center">
+                <h1 class="text-4xl font-bold tracking-tight text-gray-900 dark:text-gray-200 sm:text-6xl">Data to
+                    enrich your online business</h1>
+                <p class="mt-6 text-lg leading-8 text-gray-600 dark:text-gray-400">Lorem ipsum dolor sit amet
+                    consectetur adipisicing elit. Iure debitis odio libero nihil! Aspernatur delectus natus aperiam,
+                    consectetur repudiandae consequuntur quidem omnis cupiditate minima repellat dolorum neque
+                    doloremque odio tempore.</p>
+                <div class="mt-10 flex items-center justify-center gap-x-6">
+                    <button
+                        class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                        Get started
+                    </button>
+                    <a href="#"
+                        class="text-sm font-semibold leading-6 text-gray-900 dark:text-gray-400 hover:text-indigo-600 dark:hover:text-indigo-400 hover:underline">Learn
+                        more <span aria-hidden="true">&rarr;</span></a>
+                </div>
+            </div>
+        </x-section>
     </div>
 </x-app-layout>
